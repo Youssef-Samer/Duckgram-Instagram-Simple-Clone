1b10e08d72d7d96a616be5113487daf6d070afe5
Youssef Samer <youssefsamer122@gmail.com>
2022-07-26 02:36:10 +0200

    Duckgram Instagram Clone

---
+++ IAExternalDb/Controllers/PostController.cs
@@ -0,0 +1,51 @@
+﻿using IAExternalDb.Context;
+using IAExternalDb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IAExternalDb.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        DataContext db = new DataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /* [HttpGet]
+         public int Like(int indexofimage)
+         {
+             User user = db.UserTable.ToList().SingleOrDefault(x => x.user_id == Int32.Parse(Session["CurrentUser"].ToString()));
+             Like newLike = new Like()
+             {
+                 Post_ID = user.PostsList.ToList()[indexofimage].id,
+                 like_owner_id = user.user_id
+             };
+             db.LikeTable.Add(newLike);
+             db.SaveChanges();
+             return user.PostsList.ToList()[indexofimage].LikesList.Count;
+         }*/
+        [HttpGet]
+        public int Like (int postID)
+        {
+            Like newLike = new Like()
+            {
+                Post_ID = postID,
+                like_owner_id = Int32.Parse(Session["CurrentUser"].ToString())
+            };
+            db.LikeTable.Add(newLike);
+            db.SaveChanges();
+            return db.PostTable.ToList().SingleOrDefault(x => x.id == postID).LikesList.Count;
+        }
+
+        public ActionResult showPost(int id)
+        {
+            return View(db.PostTable.SingleOrDefault(item => item.id == id));
+        }
+
+    }
+}
---
+++ IAExternalDb/Controllers/ProfileController.cs
@@ -0,0 +1,73 @@
+﻿using IAExternalDb.Context;
+using IAExternalDb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IAExternalDb.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        DataContext db = new DataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult UserProfile(int id)
+        {
+            User user = db.UserTable.ToList().SingleOrDefault(x => x.user_id == id);
+            return View(user);
+        }
+        [HttpGet]
+        public ActionResult myProfile()
+        {
+            User CurrentLoggedin = Session["CurrentUser"] as IAExternalDb.Models.User;
+            User user = db.UserTable.ToList().SingleOrDefault(x => x.user_id == CurrentLoggedin.user_id);
+            return View(user);
+        }
+        [HttpGet]
+        public ActionResult EditProfile()
+        {
+            User CurrentLoggedin = Session["CurrentUser"] as IAExternalDb.Models.User;
+            User user = db.UserTable.ToList().SingleOrDefault(x => x.user_id == CurrentLoggedin.user_id);
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult EditProfile(User user, HttpPostedFileBase file)
+        {
+            User CurrentLoggedin = Session["CurrentUser"] as IAExternalDb.Models.User;
+            var St = db.UserTable.SingleOrDefault(x => x.user_id == CurrentLoggedin.user_id);
+            if (file != null)
+            {
+                string pic = System.IO.Path.GetFileName(file.FileName);
+                string path = System.IO.Path.Combine(Server.MapPath("~/Images/ProfilePictures/"), pic);
+
+                file.SaveAs(path);
+
+                St.ProfilePicture = pic;
+            }
+            St.Fname = user.Fname;
+            St.Lname = user.Lname;
+            St.Telephone = user.Telephone;
+            St.Password = user.Password;
+
+            db.Entry(St).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("myProfile");
+        }
+        [HttpGet]
+        public ActionResult ShowFollowers(int id)
+        {
+            return View(db.UserTable.ToList().SingleOrDefault(x => x.user_id == id).FollowersList.ToList());
+        }
+        [HttpGet]
+        public ActionResult ShowFollowing(int id)
+        {
+            return View(db.UserTable.ToList().SingleOrDefault(x => x.user_id == id).FollowingList.ToList());
+        }
+    }
+}
